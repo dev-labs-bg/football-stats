9cdfcbd3fefb3298beeca9bf68bc6c44d5acb17f
Veselin Bratanov <veselin@devlabs.bg>
2016-09-17 17:48:17 +0300

    Improved average coordinates function - maximum distance to eliminate errors

--- average_coordinates.py
+++ average_coordinates.py
@@ -1,10 +1,19 @@
 previousTen = []
-def getRunningAverageCoordinates(basepoint):
+def getRunningAverageCoordinates(basepoint, historySize = 10, maxDistance = 100):
+	outsideOfMaxDistanceRange = False
 
-	previousTen.insert(0, basepoint)
+	# calculate distance from previous basepoint
+	if len(previousTen) > 0:
+		previousPoint = previousTen[0]
+		distance = abs(previousPoint[0] - basepoint[0]), abs(previousPoint[1] - basepoint[1])
+		if distance[0] > maxDistance or distance[1] > maxDistance:
+			outsideOfMaxDistanceRange = True
 
-	# remove if more than 10 items
-	del previousTen[10:]
+	if outsideOfMaxDistanceRange == False:
+		# insert in the coordinate history
+		previousTen.insert(0, basepoint)
+		# remove older history records
+		del previousTen[historySize:]
 
 	# get average from all points in previousTen
 	avgX = sum([basepoint[0] for basepoint in previousTen]) / len(previousTen)
